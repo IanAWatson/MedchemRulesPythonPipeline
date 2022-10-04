12db1da1f1381b02d29fefc62d52910c0ae0b5be
Ian Watson <ianiwatson@gmail.com>
2022-10-04 09:35:10 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MedchemRulesPythonPipeline
 Demonstration of calling the Lilly Medchem Rules from python using subprocess.
+
+Does not work as currently implemented, but if the path is adjusted to the location
+on your system, it should work.
+
+The main finding is that there seems to be minimal overhead using this approach
+when working in python.
---
+++ pipelined_medchem_rules.py
@@ -0,0 +1,34 @@
+"""Toy app showing how to run the Lilly Medchem Rules from a
+python subprocess.
+On older hardware,
+Intel(R) Core(TM) i7-8700K CPU @ 3.70GHz
+and with 4.87M molecules it takes about 9 minutes.
+The same command, running outside python, takes about
+the same time, so there is little discernable penalty
+for rnning inside python using subprocess.
+"""
+
+import logging, subprocess, sys
+
+def run_medchem_rules(fname: str):
+  cmd = f'/path/to/Lilly_Medchem_Rules.rb {fname}'
+  p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, shell=True)
+  for line in iter(p.stdout.readline, b''):
+    yield line.decode('utf-8')
+
+def pipelined_medchem_rules(fname: str):
+  for line in run_medchem_rules(fname):
+    print(line.rstrip())
+
+
+def main(argv):
+  """Demonstrate running Lilly Medchem Rules via pipeline"""
+
+  if len(argv) == 1:
+    logging.fatal('Must specify smiles file on command line')
+
+  pipelined_medchem_rules(argv[1])
+
+if __name__ == '__main__':
+  main(sys.argv)
